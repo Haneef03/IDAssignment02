ce589bfdbb69bd90daf7d503334860ec3deac23d
Haneef03 <s10223481@connect.np.edu.sg>
2022-02-12 17:18:50 +0800

    Copied Viego's framwork and basically all of it

---
+++ script.js
@@ -0,0 +1,28 @@
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  /* Link to variants*/
+  function exploreBudget() {
+    window.location.href="budgetBuild.html";
+  }
+  function exploreLudicrous() {
+    window.location.href="ludicrousBuild.html";
+  }
+  
+  window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-170px";
+  }
+}
