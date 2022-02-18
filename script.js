5cb4ab6bee4a1b32a3852e72d9ca54f196798946
Gabriel-Koh <94812128+Gabriel-Koh@users.noreply.github.com>
2022-02-18 17:11:03 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -26,8 +26,41 @@ function scrollFunction() {
     document.getElementById("navbar").style.top = "-170px";
   }
 }
-/* Link to checkout*/
-function checkOut() {
-  window.open("checkOutPageMain.html", '_blank').focus();
+
+function Prize1() {
+  return "CODE10:"+ Math.floor(Math.random() * 1000)+Math.floor(Math.random() * 1000)+Math.floor(Math.random() * 1000)
 }
 
+function Prize2() {
+  return "CODE20:"+ Math.floor(Math.random() * 1000)+Math.floor(Math.random() * 1000)+Math.floor(Math.random() * 1000)
+}
+
+function Prize3() {
+  return "CODE30:"+ Math.floor(Math.random() * 1000)+Math.floor(Math.random() * 1000)+Math.floor(Math.random() * 1000)
+}
+
+function hidediv(){
+  document.getElementById('h1').style.visibility = "hidden";
+}
+setTimeout(hidediv(), 5000)
+
+function luckyDraw() {
+  randNum = Math.floor(Math.random() * 1000)
+  if (randNum >=0 && randNum <= 500) {
+    document.querySelector('h1').innerHTML =  Prize1() /*10% voucher*/
+  } 
+  else if (randNum >500 && randNum <= 800) {
+    document.querySelector('h1').innerHTML = Prize2() /*20% voucher*/
+  }
+  else if (randNum >800 && randNum <= 1000) {
+    document.querySelector('h1').innerHTML = Prize3() /*30% voucher*/
+  }
+}
+
+var animation = bodymovin.loadanimatiion({
+  container: document.getElementById('anim'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://assets2.lottiefiles.com/packages/lf20_reqivhma.json'
+})
